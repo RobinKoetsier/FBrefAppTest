4ee0165396fb931c63960788839d9d57cb53730c
Robin Koetsier <45625446+RobinKoetsier@users.noreply.github.com>
2020-08-20 20:01:57 +0200

    Add files via upload

--- app.R
+++ app.R
@@ -9,14 +9,30 @@ library(glue)
 source("HelpersFBREF.R")
 ga_set_tracking_id("UA-175572271-1")
 ga_set_approval(consent = TRUE)
-ga_set_tracking_id("UA-170459986-1")
-ga_set_approval(consent = TRUE)
+
 ELCL <- readRDS("fbrefdata.rds")
 
 
 ChoicesList <- colnames(ELCL)[c(3:55)]
 ChoicesList <- sort(ChoicesList)
-ui <- fluidPage(
+ui <- fluidPage(tags$head(HTML(
+  "<script>
+      (function(i,s,o,g,r,a,m){
+        i['GoogleAnalyticsObject']=r;i[r]=i[r]||
+        function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
+          a=s.createElement(o), m=s.getElementsByTagName(o)[0];
+          a.async=1;
+          a.src=g;m.parentNode.insertBefore(a,m)
+        })
+      (window, document, 'script',
+        '//www.google-analytics.com/analytics.js','ga');
+      
+        ga('create', 'UA-175572271-1', 'auto');
+        ga('send', 'pageview');
+      
+      </script>"
+)),
     
     # Application title
     titlePanel("Create your own FBref scatter plot - A Shiny app by @RobinWilhelmus"),
